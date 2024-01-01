6bf3d227e49fa8fc2141800c40727cc7433e4efe
NagarjunaReddy”git config --global user.name NagarjunaReddy” <Nagarjuna1@email.com>
2024-01-01 16:32:55 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+
+    <div className="max-width: 300px; margin: 0 auto d-flex justify-content-center align-items-center bg-secondary vh-100">
+      <div>
+        <p>Completion of work</p>
+        <p>Do home work</p>
+
+        </div>
+    </div>
+
+  )
+}
+
+export default Home
